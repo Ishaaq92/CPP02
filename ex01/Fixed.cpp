4b035f53013adbd5b76fd7fc62768b4fac2749e2
ishaaq <a.ishaaq@icloud.com>
2025-08-19 15:50:22 +0100

    not working but progress

--- ex01/Fixed.cpp
+++ ex01/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: isahmed <isahmed@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/18 11:13:48 by isahmed           #+#    #+#             */
-/*   Updated: 2025/08/19 15:30:16 by isahmed          ###   ########.fr       */
+/*   Updated: 2025/08/19 15:47:05 by isahmed          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,6 @@ void	Fixed::setRawBits(int const raw)
 
 Fixed	&Fixed::operator=(const Fixed &right)
 {
-	
 	std::cout << "Copy assignment operator called" << std::endl;
 	this->value_ = right.getRawBits();
 	return (*this);
@@ -53,29 +52,26 @@ Fixed	&Fixed::operator=(const Fixed &right)
 Fixed::Fixed(const int val)
 {
 	std::cout << "Int constructor called" << std::endl;
-	this->value_ = val;
+	this->value_ = (val << this->fractional_);
 }
 
 Fixed::Fixed(const float val)
 {
 	std::cout << "Float constructor called" << std::endl;
-	this->value_ = (int) val;
+	this->value_ = val << this->fractional_;
 }
 
 float	Fixed::toFloat( void ) const
 {
 	float	val;
-	
-	val = (float) this->value_;
+
+	val = (float) (this->value_ >> this->fractional_);
 	return (val);
 }
 
 int		Fixed::toInt( void ) const
 {
-	int	val;
-
-	val = this->value_;
-	return (val);
+	return (this->value_ >> this->fractional_);
 }
 
 std::ostream	&operator<<(std::ostream &out, Fixed &val)
